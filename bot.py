fb6b9057fbed4d17d3ad4244bfc00cc173fd0986
Matheus <matheusfterra@hotmail.com>
2020-03-23 02:45:09 -0300

    Configuracoes Iniciais

--- bot.py
+++ bot.py
@@ -1,6 +1,15 @@
-import spacy
+from rasa_nlu.training_data import load_data
+from rasa_nlu.model import Trainer
+from rasa_nlu import config
+from rasa_nlu.model import Interpreter
 
-nlp = spacy.load('en') #Loads the spacy en model into a python object
-doc = nlp(u'I am learning how to build chatbots') #Creates a doc object
-for token in doc:
-    print(token.text, token.pos_) #prints the text and POS
+def train_horoscopebot(data_json, config_file, model_dir):
+    training_data = load_data(data_json)
+    trainer = Trainer(config.load(config_file))
+    trainer.train(training_data)
+    model_directory = trainer.persist(model_dir, fixed_model_name =
+'horoscopebot')
+
+def predict_intent(text):
+    interpreter = Interpreter.load('./models/nlu/default/horoscopebot')
+    print(interpreter.parse(text))
